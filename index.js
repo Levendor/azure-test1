9655498ac1affba5d8f7104506b4c2917d18879e
Levendor <myth_of_lavanda@mail.ru>
2023-07-17 14:42:09 +0200

    feat: html output

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import { createServer } from 'http';
 const port = process.env.PORT || 3000;
 
 createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World!');
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.end("<html><body><h1>Hello World!</h1></body></html>");
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
