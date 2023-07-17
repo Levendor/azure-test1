08f5e07436d29870f3b0cd59bd04861e76e49502
Andrei Chaitsyn <andrei.chaitsyn@headspace.com>
2023-07-17 16:35:26 +0200

    fix: stringify json

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const port = process.env.PORT || 3000;
 
 createServer(function(request, response) {
   response.writeHead(200, { "Content-Type": "application/json" });
-  response.end({
+  response.end(JSON.stringify({
     "message": "Hello, World!"
-  });
+  }));
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
